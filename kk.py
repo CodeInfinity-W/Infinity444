7b661c1df1cafb645523972779242042e7ff37f2
CodeInfinity-W <adjanivision@gmail.com>
2025-01-09 21:58:04 +0000

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,16 @@
+import calendar
+from datetime import datetime
+
+
+datetime_object = datetime.now()
+print(datetime_object)
+
+week_num = calendar.weekday(2025,1,12)
+week_days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+print(week_days[week_num])
+
+if week_num == calendar.weekday(2025,1,8):
+    print("You have recieved $.50")
+
+else:
+    print("Error")
